71f88d665357a15d6b90087dc510aa0c291b3568
Anuj Kumar Chaudhary <kumar_anuj87@yahoo.co.in>
2018-04-21 07:28:16 +0530

    Bootstraped and redirect fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,10 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+       if (!\Auth::guest() and !\System::isGuestCreated()) {
+            return redirect(config('system.auth.redirect','/'));
         }
-
         return $next($request);
     }
 }
